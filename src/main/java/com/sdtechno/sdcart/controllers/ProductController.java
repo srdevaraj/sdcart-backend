f19d77f0472731303b06856fb324d88f62c06519
srdevaraj2006 <srlaladev@gmail.com>
2025-07-20 13:59:36 +0530

    i removed one line in productcontroller which is protect that api...

--- src/main/java/com/sdtechno/sdcart/controllers/ProductController.java
+++ src/main/java/com/sdtechno/sdcart/controllers/ProductController.java
@@ -80,7 +80,6 @@ public class ProductController {
 
     // ✅ 4. Get full product details by ID (USER or ADMIN)
     @GetMapping("/product/{id}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')") // <-- Added this line
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         Optional<Product> product = productservice.getProductById(id);
         return product.map(ResponseEntity::ok)
